394a427c14cc9e4c0b9299b867b80262e3a1f4d0
Yahyo <abdukarimovyahyo7@gmail.com>
2026-05-19 20:12:30 +0500

    this simple javascript cod

---
+++ script.js
@@ -0,0 +1,19 @@
+let misol_1 = 25/5;
+console.log("misol_1 = " + misol_1);
+//natija: 5
+
+let misol_2 = 500/10;
+console.log("misol_2 = " + misol_2);
+//natija: 50
+
+let misol_3 = 105/5;
+console.log("misol_3 = " + misol_3);
+//natija: 21
+
+let misol_4 = 100/20;
+console.log("misol_4 = " + misol_4);
+//natija: 5
+
+let misol_5 = 55/5;
+console.log("misol_5 = " + misol_5);
+//natija: 11
